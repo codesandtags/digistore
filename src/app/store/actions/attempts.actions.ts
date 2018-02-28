a2610a1ca953f7aa960d0f5e632cb9ccea551451
Diego Hoyos <hoyosd@colpatria.com>
2018-02-28 16:56:20 -0500

    hoyosd - Todos

--- src/app/store/actions/attempts.actions.ts
+++ src/app/store/actions/attempts.actions.ts
@@ -1,14 +1,21 @@
-// Action Constants
+import { Action } from '@ngrx/store';
+import { Attempt } from '../reducers/attempts.reducer';
 
-// TODO #1 : Define the ADD_ATTEMPT and RESET_ATTEMPT actions
+// Action Constants
 export const RESET_ATTEMPTS = '[Attempts] Reset Attempts';
+export const ADD_ATTEMPT = '[Attempts] Add Attempt';
 
 // Action Creators
-// TODO #2 : Define the AddAttempt and ResetAttempts implementing the Action (@ngrx/store) interface
-export class ResetAttempts {
+export class ResetAttempts implements Action {
     readonly type = RESET_ATTEMPTS;
 }
 
+export class AddAttempt implements Action {
+    readonly type = ADD_ATTEMPT;
+    constructor(public payload: Attempt) {
+
+    }
+}
+
 // Action Types
-// TODO #3: Complete types for the Attempts Actions
-export type AttemptsActions = ResetAttempts;
+export type AttemptsActions = ResetAttempts | AddAttempt;
