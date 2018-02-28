262b3c1408df1939fc56769f491500b79b5a2f17
Carlos Carvajal <carvajac@colpatria.com>
2018-02-28 16:59:33 -0500

    Todos 7

--- src/app/store/actions/attempts.actions.ts
+++ src/app/store/actions/attempts.actions.ts
@@ -2,18 +2,18 @@ import { Action } from '@ngrx/store';
 // Action Constants
 
 // TODO #1 : Define the ADD_ATTEMPT and RESET_ATTEMPT actions
-export const ADD_ATTEMPTS='Add attempts' 
+export const ADD_ATTEMPT='Add attempts' 
 
-export const RESET_ATTEMPTS = '[Attempts] Reset Attempts';
+export const RESET_ATTEMPT = '[Attempts] Reset Attempts';
 
 // Action Creators
 // TODO #2 : Define the AddAttempt and ResetAttempts implementing the Action (@ngrx/store) interface
 export class ResetAttempts implements Action {
-    readonly type = RESET_ATTEMPTS;
+    readonly type = RESET_ATTEMPT;
 }
 
 export class AddAttempts implements Action {
-    readonly type = ADD_ATTEMPTS;
+    readonly type = ADD_ATTEMPT;
 }
 
 // Action Types
